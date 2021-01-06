3e372579fa31b871caf9d46395b8809d95af0cc1
Amishmaqbool <Amishmaqbool@gmail.com>
2021-01-06 10:52:53 +0500

    configs added

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,15 +1,14 @@
 const awsmobile = {
-  awsj_proect_region: project_region,
-  aws_cognito_identity_pool_id: cognito_identity_pool_id,
-  aws_cognito_region: cognito_region,
-  aws_user_pools_id: user_pools_id,
-  aws_user_pools_web_client_id: user_pools_web_client_id,
+    "aws_proect_region":process.env.project_region,
+    "aws_cognito_identity_pool_id":process.env.cognito_identity_pool_id,
+    "aws_cognito_region":process.env.cognito_region,
+    "aws_user_pools_id":process.env.user_pools_id,
+    "aws_user_pools_web_client_id":process.env.user_pools_web_client_id,
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://qg7llzyygraabddjgd3auazbki.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS"
+    };
 
-  oauth: {},
-  aws_appsync_graphqlEndpoint:
-    "https://qg7llzyygraabddjgd3auazbki.appsync-api.us-east-1.amazonaws.com/graphql",
-  aws_appsync_region: "us-east-1",
-  aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS",
-};
 
-export default awsmobile;
+export default awsmobile; 
